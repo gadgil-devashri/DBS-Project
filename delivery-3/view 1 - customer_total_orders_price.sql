949fb011e86239cba052e203501e5028d1bc49ad
MohammedAghil <57040494+MohammedAghil@users.noreply.github.com>
2021-12-08 16:04:19 -0500

    Views

--- delivery-3/view 1 - customer_total_orders_price.sql
+++ delivery-3/view 1 - customer_total_orders_price.sql
@@ -1,7 +1,7 @@
 -- View 1 : Display the total price of the orders by each customer
 USE `campus_eats_fall2020`;
-CREATE  OR REPLACE VIEW `customer_orders` AS
+CREATE  OR REPLACE VIEW `customer_total_orders_price` AS
 SELECT DISTINCT person_id as customer_id,
-    ROUND(total_price + delivery_charge, 2) AS order_total
+    ROUND(total_price + delivery_charge, 2) AS orders_total
     FROM campus_eats_fall2020.order 
     GROUP BY person_id;;
