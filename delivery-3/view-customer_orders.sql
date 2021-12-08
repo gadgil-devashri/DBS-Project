3c8d18d75a42371a1e50795622e3ed4bfe351655
MohammedAghil <57040494+MohammedAghil@users.noreply.github.com>
2021-12-08 00:35:40 -0500

    Update view-customer_orders.sql

---
+++ delivery-3/view-customer_orders.sql
@@ -0,0 +1,6 @@
+USE `campus_eats_fall2020`;
+CREATE  OR REPLACE VIEW `customer_orders` AS
+SELECT DISTINCT person_id as customer_id,
+    ROUND(total_price + delivery_charge, 2) AS order_total
+    FROM campus_eats_fall2020.order 
+    GROUP BY person_id;;
