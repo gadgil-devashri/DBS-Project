458b030d038c7b3b9cc7f7d616d4cf9c3b0ba981
Ramakrishna <kolipakarama98@gmail.com>
2021-12-08 17:20:41 -0500

    Create driver_rating_function.sql

---
+++ delivery-3/driver_rating_function.sql
@@ -0,0 +1,26 @@
+DROP FUNCTION IF EXISTS driver_rating_func;
+DELIMITER //
+CREATE FUNCTION driver_rating_func
+(
+ rating INT
+)
+RETURNS varchar(100) 
+DETERMINISTIC
+BEGIN
+    DECLARE rating_text varchar(100);
+    IF rating = 1 THEN
+		SET rating_text = "Bad driver";
+	ELSEIF rating = 2 THEN
+		SET rating_text = "Average driver";
+	ELSEIF rating = 3 THEN
+		SET rating_text = "Moderate driver";
+	ELSEIF rating = 4 THEN
+		SET rating_text = "Good driver";
+	ELSEIF rating = 5 THEN
+		SET rating_text = "Expert driver";
+	END IF;
+    RETURN rating_text; 
+END//
+DELIMITER ; 
+
+select driver_rating_func(rating) from driver;
